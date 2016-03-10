4b861239deaf3682175c8060b47a6bd73fe18cc1
Juraj Masar <mail@jurajmasar.com>
2016-03-10 12:22:33 +0100

    Check if String.blank? is already defined.

--- lib/oror.rb
+++ lib/oror.rb
@@ -1,21 +1,23 @@
-# from https://github.com/rails/rails/blob/42b0c942520e59399d70c2170253aa5275a42af1/activesupport/lib/active_support/core_ext/object/blank.rb#L115
-class String
-  BLANK_RE = /\A[[:space:]]*\z/
+unless String.method_defined? :blank?
+  # from https://github.com/rails/rails/blob/42b0c942520e59399d70c2170253aa5275a42af1/activesupport/lib/active_support/core_ext/object/blank.rb#L115
+  class String
+    BLANK_RE = /\A[[:space:]]*\z/
 
-  # A string is blank if it's empty or contains whitespaces only:
-  #
-  #   ''.blank?       # => true
-  #   '   '.blank?    # => true
-  #   "\t\n\r".blank? # => true
-  #   ' blah '.blank? # => false
-  #
-  # Unicode whitespace is supported:
-  #
-  #   "\u00a0".blank? # => true
-  #
-  # @return [true, false]
-  def blank?
-    BLANK_RE === self
+    # A string is blank if it's empty or contains whitespaces only:
+    #
+    #   ''.blank?       # => true
+    #   '   '.blank?    # => true
+    #   "\t\n\r".blank? # => true
+    #   ' blah '.blank? # => false
+    #
+    # Unicode whitespace is supported:
+    #
+    #   "\u00a0".blank? # => true
+    #
+    # @return [true, false]
+    def blank?
+      BLANK_RE === self
+    end
   end
 end
 
