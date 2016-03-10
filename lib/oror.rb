b5f76294503d68ce60b90e132bbe02fbab5c28a7
Juraj Masar <mail@jurajmasar.com>
2016-03-10 11:54:27 +0100

    Addition of specs.

--- lib/oror.rb
+++ lib/oror.rb
@@ -1,6 +1,27 @@
+# from https://github.com/rails/rails/blob/42b0c942520e59399d70c2170253aa5275a42af1/activesupport/lib/active_support/core_ext/object/blank.rb#L115
+class String
+  BLANK_RE = /\A[[:space:]]*\z/
+
+  # A string is blank if it's empty or contains whitespaces only:
+  #
+  #   ''.blank?       # => true
+  #   '   '.blank?    # => true
+  #   "\t\n\r".blank? # => true
+  #   ' blah '.blank? # => false
+  #
+  # Unicode whitespace is supported:
+  #
+  #   "\u00a0".blank? # => true
+  #
+  # @return [true, false]
+  def blank?
+    BLANK_RE === self
+  end
+end
+
 class Object
   def oror(alternative)
-    if respond_to?(:empty?) && !!empty? || !self
+    if respond_to?(:blank?) && !!blank? || respond_to?(:empty?) && !!empty? || !self
       alternative
     else
       self
--- spec/oror_spec.rb
+++ spec/oror_spec.rb
@@ -1,7 +1,18 @@
 require_relative '../lib/oror'
 
 describe 'oror' do
-  it 'should return the first value if not blank' do
-    expect(47.oror 48).to eq 47
+  shared_examples_for 'oror' do |object, alternative, result|
+    it "#{object}.oror #{alternative} == #{result}" do
+      expect(object.oror alternative).to eq result
+    end
   end
+
+  it_behaves_like 'oror', 47, 48, 47
+  it_behaves_like 'oror', '', 'alternative', 'alternative'
+  it_behaves_like 'oror', nil, 'alternative', 'alternative'
+  it_behaves_like 'oror', false, 'alternative', 'alternative'
+  it_behaves_like 'oror', ' ', 'alternative', 'alternative'
+  it_behaves_like 'oror', true, 'alternative', true
+  it_behaves_like 'oror', 47.47, 'alternative', 47.47
+  it_behaves_like 'oror', "\t\n\r", 'alternative', 'alternative'
 end
